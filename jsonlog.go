079225f42317f6e5e3d6a8e3ec92812e196a9432
ltsrad <alex+rad@longterm.io>
2022-02-08 11:48:54 -0800

    Update formatting

--- jsonlog.go
+++ jsonlog.go
@@ -10,23 +10,22 @@ influxdb http://192.168.0.193:8086/ test dns_data base64keyhere==
 package jsonlog
 
 import (
-  "context"
+	"context"
 	"encoding/json"
 	"fmt"
 	"strings"
 	"time"
 
-  "github.com/coredns/caddy"
+	"github.com/coredns/caddy"
 	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/plugin"
 	"github.com/miekg/dns"
 
-  "github.com/jackc/pgx/v4/pgxpool"
-  "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/influxdata/influxdb-client-go/v2"
+	"github.com/jackc/pgx/v4/pgxpool"
 
-  clog "github.com/coredns/coredns/plugin/pkg/log"
+	clog "github.com/coredns/coredns/plugin/pkg/log"
 )
-
 
 const (
 	coreDNSPackageName string = `jsonlog`
@@ -41,69 +40,67 @@ func init() {
 	})
 }
 
-
 // JsonLog is the  plugin.
 type JsonLog struct {
-  SQL *pgxpool.Pool
-  IFDB influxdb2.Client
-  IFDB_org string
-  IFDB_bucket string
-	Next plugin.Handler
+	SQL         *pgxpool.Pool
+	IFDB        influxdb2.Client
+	IFDB_org    string
+	IFDB_bucket string
+	Next        plugin.Handler
 }
 
 func New() *JsonLog {
-	return &JsonLog{
-	}
+	return &JsonLog{}
 }
 
 func setup(c *caddy.Controller) error {
 
-  jsonlog := New()
+	jsonlog := New()
 
 	for c.Next() {
-    for c.NextBlock() {
-      var arg, val string
-      arg = c.Val();
-      c.NextArg()
-      val = c.Val()
-      c.NextArg()
-      switch arg {
-        case `pgdb`: {
-          conn, err := pgxpool.Connect(context.Background(), val)
-          if err != nil {
-        		panic("Unable to connect to database")
-        	}
-          jsonlog.SQL = conn
-        }
-        case `influxdb`: {
-          org := c.Val()
-          c.NextArg()
-          bucket := c.Val()
-          c.NextArg()
-          token := c.Val()
-          c.NextArg()
-          jsonlog.IFDB = influxdb2.NewClient(val, token) //tbd keep handle around to later close it
-          jsonlog.IFDB_org = org
-          jsonlog.IFDB_bucket = bucket
-        }
-      }
-    }
-  }
+		for c.NextBlock() {
+			var arg, val string
+			arg = c.Val()
+			c.NextArg()
+			val = c.Val()
+			c.NextArg()
+			switch arg {
+			case `pgdb`:
+				{
+					conn, err := pgxpool.Connect(context.Background(), val)
+					if err != nil {
+						panic("Unable to connect to database")
+					}
+					jsonlog.SQL = conn
+				}
+			case `influxdb`:
+				{
+					org := c.Val()
+					c.NextArg()
+					bucket := c.Val()
+					c.NextArg()
+					token := c.Val()
+					c.NextArg()
+					jsonlog.IFDB = influxdb2.NewClient(val, token) //tbd keep handle around to later close it
+					jsonlog.IFDB_org = org
+					jsonlog.IFDB_bucket = bucket
+				}
+			}
+		}
+	}
 
-  if jsonlog.SQL == nil && jsonlog.IFDB == nil {
-    log.Fatal("no   connection")
-    return nil
-  }
+	if jsonlog.SQL == nil && jsonlog.IFDB == nil {
+		log.Fatal("no   connection")
+		return nil
+	}
 
 	dnsserver.GetConfig(c).AddPlugin(func(next plugin.Handler) plugin.Handler {
 		jsonlog.Next = next
 		return jsonlog
 	})
 
-
 	return nil
 }
-
 
 func (plugin JsonLog) String() string {
 	return coreDNSPackageName
@@ -114,18 +111,18 @@ func (plugin JsonLog) Name() string {
 }
 
 type EventData struct {
-  Q []dns.Question
-	A []dns.RR
-  FirstName string
-  FirstAnswer string
-  Local string
-  Remote string
-  Timestamp time.Time
+	Q           []dns.Question
+	A           []dns.RR
+	FirstName   string
+	FirstAnswer string
+	Local       string
+	Remote      string
+	Timestamp   time.Time
 }
 
 type DNSEvent struct {
 	dns.ResponseWriter
-  data EventData
+	data EventData
 }
 
 func (i *DNSEvent) Write(b []byte) (int, error) {
@@ -139,8 +136,8 @@ func (i *DNSEvent) WriteMsg(m *dns.Msg) error {
 }
 
 func (i *DNSEvent) String() string {
-  x, _ := json.Marshal(i.data)
-  return string(x)
+	x, _ := json.Marshal(i.data)
+	return string(x)
 }
 
 func (plugin JsonLog) ServeDNS(ctx context.Context, rw dns.ResponseWriter, r *dns.Msg) (c int, err error) {
@@ -150,55 +147,54 @@ func (plugin JsonLog) ServeDNS(ctx context.Context, rw dns.ResponseWriter, r *dn
 	event := &DNSEvent{
 		ResponseWriter: rw,
 	}
-  event.data.Timestamp = time.Now()
-  event.data.Local = local.String()
-  event.data.Remote = remote.String()
-
+	event.data.Timestamp = time.Now()
+	event.data.Local = local.String()
+	event.data.Remote = remote.String()
 
 	c, err = plugin.Next.ServeDNS(ctx, event, r)
 
-  if len(event.data.Q) >= 1 {
-    //set FirstName
-    event.data.FirstName = event.data.Q[0].Name
-  }
+	if len(event.data.Q) >= 1 {
+		//set FirstName
+		event.data.FirstName = event.data.Q[0].Name
+	}
 
-  if len(event.data.A) >= 1 {
-    //set FirstName
-    event.data.FirstName = event.data.A[0].Header().Name
+	if len(event.data.A) >= 1 {
+		//set FirstName
+		event.data.FirstName = event.data.A[0].Header().Name
 
-    //Answers can be A, AAAA, CNAME, etc. To simplify things get the string form
-    //and use the separator. FirstName/FirstString will make it easy to classify results downstream
+		//Answers can be A, AAAA, CNAME, etc. To simplify things get the string form
+		//and use the separator. FirstName/FirstString will make it easy to classify results downstream
 
-    //TBD ... maybe pick the ip address as the first answer?
-    answerString := event.data.A[0].String()
-    parts := strings.Split(answerString, "\t")
-    event.data.FirstAnswer = parts[len(parts)-1]
-  }
+		//TBD ... maybe pick the ip address as the first answer?
+		answerString := event.data.A[0].String()
+		parts := strings.Split(answerString, "\t")
+		event.data.FirstAnswer = parts[len(parts)-1]
+	}
 
-  plugin.PushEvent(event)
+	plugin.PushEvent(event)
 
 	return
 }
 
 func (plugin JsonLog) PushEvent(event *DNSEvent) {
 
-  if (plugin.SQL != nil) {
-    _, err := plugin.SQL.Exec(context.Background(), "INSERT INTO dns(data) VALUES(?)", event.String())
-  	if err != nil {
-      fmt.Println("exec tx")
-  		log.Fatal(err)
-  	}
-  } else if (plugin.IFDB != nil) {
-    writeAPI := plugin.IFDB.WriteAPI(plugin.IFDB_org, plugin.IFDB_bucket)
+	if plugin.SQL != nil {
+		_, err := plugin.SQL.Exec(context.Background(), "INSERT INTO dns(data) VALUES(?)", event.String())
+		if err != nil {
+			fmt.Println("exec tx")
+			log.Fatal(err)
+		}
+	} else if plugin.IFDB != nil {
+		writeAPI := plugin.IFDB.WriteAPI(plugin.IFDB_org, plugin.IFDB_bucket)
 
-    p := influxdb2.NewPointWithMeasurement("dns").
-      AddField("FirstName", event.data.FirstName).
-      AddField("FirstAnswer", event.data.FirstAnswer).
-      AddField("Remote", event.data.Remote).
-      AddField("Local", event.data.Local)
+		p := influxdb2.NewPointWithMeasurement("dns").
+			AddField("FirstName", event.data.FirstName).
+			AddField("FirstAnswer", event.data.FirstAnswer).
+			AddField("Remote", event.data.Remote).
+			AddField("Local", event.data.Local)
 
-    writeAPI.WritePoint(p)
-    writeAPI.Flush()
-  }
+		writeAPI.WritePoint(p)
+		writeAPI.Flush()
+	}
 
 }
